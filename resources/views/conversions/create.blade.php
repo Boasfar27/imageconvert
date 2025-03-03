13e5e2132ed8c588d3a87611b913c58d180a6129
Boasfar27 <muhammadfarhan2747@gmail.com>
2025-03-04 02:50:46 +0700

    fix deploy

--- resources/views/conversions/create.blade.php
+++ resources/views/conversions/create.blade.php
@@ -7,7 +7,7 @@
                 </svg>
                 {{ __('Convert New Images') }}
             </h2>
-            <a href="{{ route('conversions.index') }}" class="flex items-center px-4 py-2 text-sm font-medium text-gray-600 bg-white dark:bg-gray-800 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <a href="{{ route('conversions.index') }}" class="flex items-center px-4 py-2 text-sm font-medium text-white bg-white dark:bg-gray-800 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 17l-5-5m0 0l5-5m-5 5h12"/>
                 </svg>
