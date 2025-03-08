af539ff89bb0aac2d38e382ca245efba2bb42196
Boasfar27 <muhammadfarhan2747@gmail.com>
2025-03-09 00:49:39 +0700

    fix google adsense

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,10 @@
 
         <title>{{ config('app.name', 'ImageConverter') }}</title>
 
+        <!-- Google AdSense -->
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7311209549685817"
+     crossorigin="anonymous"></script>
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
